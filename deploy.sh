84f318e0472934ec2031edad252f67d6820d3835
Nikita Kozlov <kozlov_nikita@outlook.com>
2021-08-23 15:06:48 +0300

    deploy

--- deploy.sh
+++ deploy.sh
@@ -9,9 +9,10 @@ npm run build
 # переход в каталог сборки
 cd dist
 
-# если вы публикуете на пользовательский домен
-# echo 'www.example.com' > CNAME
+git add -A
+git commit -m 'deploy'
 
 git push -f git@github.com:NikitaKozlov-R/splitter.git master:gh-pages
 
 cd -
+
